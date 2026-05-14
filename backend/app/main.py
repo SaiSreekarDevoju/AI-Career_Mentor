2057050a2388604fdcf623c54a81401c4077d426
SaiSreekarDevoju <sreekardevoju6582@gmail.com>
2026-05-14 14:14:17 +0530

    added CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.init_db import init_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AI Autonomous Career Mentor API",
@@ -29,7 +30,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
